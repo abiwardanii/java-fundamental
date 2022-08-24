b326110401a24965b1d2bdec9db221a4f1f76d63
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-08-24 23:56:02 +0700

    add src

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,21 @@
+package src;
+
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 127;
+        short iniShort = 1000;
+        int iniInt = 2000;
+        long iniLong = 30000;
+        long iniLong2 = 400000;
+
+        float iniFloat = 10.12F;
+        double iniDouble = 12.2424
+
+        //literals
+        int decimalint  =20;
+        int hexint = 0xA13B;
+        int binint =  0b101010101
+
+         int amount = 1.000_2.0000()
+    }
+}
